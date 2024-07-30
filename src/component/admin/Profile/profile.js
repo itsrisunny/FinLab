c50c9578b2d3ee467c0597c6524f15f12fd38f04
lakshmiprasad-apisod <lprasad@apisod.ai>
2024-07-30 17:02:44 +0530

    Cleaning of profile page

--- src/component/admin/Profile/profile.js
+++ src/component/admin/Profile/profile.js
@@ -12,23 +12,13 @@ import { useNavigate } from "react-router-dom";
 
 export default function Profile({ menuAccess }) {
   const [offset, setOffset] = useState(0);
-  const [name, setName] = useState("");
-  const [mobileNumber, setMobileNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [crmId, setCrmId] = useState("");
-  const [employeeId, setEmployeeId] = useState("");
-  const [role, setRole] = useState("");
-  const [status, setStatus] = useState("");
+
   const [loader, setLoader] = useState(false);
   const navigate = useNavigate();
 
   const [data, setData] = useState([]);
-  const adminId = localStorage.getItem("adminId")
-  console.log("admin Id", adminId)
-
-  // const filteredData = data.filter((item) => item.id === adminId);
+  const adminId = localStorage.getItem("adminId");
   const filteredData = data.filter((item) => item.id == adminId);
-  console.log("filter data", filteredData)
 
   useEffect(() => {
     getAllProfileData(offset);
@@ -56,8 +46,6 @@ export default function Profile({ menuAccess }) {
     getAllProfileData();
   }, []);
 
-  
-
   return (
     <>
       {loader && <Loader />}
@@ -76,149 +64,141 @@ export default function Profile({ menuAccess }) {
                     <div className="authentic-add-user">
                       <div className="card">
                         <div className="card-body">
-                        {filteredData.length > 0 ? (
+                          {filteredData.length > 0 ? (
                             filteredData.map((profile) => (
-                          <form className="add-user">
-                            <div className="row">
-                              <div className="col-lg-2 col-md-2 col-sx-2">
-                                <div className="form-group">
-                                  <label htmlFor="">
-                                    Employee Id
-                                  </label>
+                              <form className="add-user">
+                                <div className="row">
+                                  <div className="col-lg-2 col-md-2 col-sx-2">
+                                    <div className="form-group">
+                                      <label htmlFor="">Employee Id</label>
+                                    </div>
+                                  </div>
+                                  <div className="col-lg-4 col-md-4 col-sx-4">
+                                    <div className="form-group">
+                                      <input
+                                        className="form-control"
+                                        type="text"
+                                        placeholder="Employee Id"
+                                        value={profile.emp_id}
+                                        readOnly
+                                      />
+                                    </div>
+                                  </div>
                                 </div>
-                              </div>
-                              <div className="col-lg-4 col-md-4 col-sx-4">
-                                <div className="form-group">
-                                  <input
-                                    className="form-control"
-                                    type="text"
-                                    placeholder="Employee Id"
-                                    value={profile.emp_id}
-                                    readOnly
-                                  />
+                                <div className="row">
+                                  <div className="col-lg-2 col-md-2 col-sx-2">
+                                    <div className="form-group">
+                                      <label htmlFor="">CRM Id</label>
+                                    </div>
+                                  </div>
+                                  <div className="col-lg-4 col-md-4 col-sx-4">
+                                    <div className="form-group">
+                                      <input
+                                        className="form-control"
+                                        type="text"
+                                        placeholder="CRM Id"
+                                        value={profile.crm_id}
+                                        readOnly
+                                      />
+                                    </div>
+                                  </div>
                                 </div>
-                              </div>
-                            </div>
-                            <div className="row">
-                              <div className="col-lg-2 col-md-2 col-sx-2">
-                                <div className="form-group">
-                                  <label htmlFor="">
-                                    CRM Id
-                                  </label>
+                                <div className="row">
+                                  <div className="col-lg-2 col-md-2 col-sx-2">
+                                    <div className="form-group">
+                                      <label htmlFor="">Name</label>
+                                    </div>
+                                  </div>
+                                  <div className="col-lg-4 col-md-4 col-sx-4">
+                                    <div className="form-group">
+                                      <input
+                                        className="form-control"
+                                        type="text"
+                                        placeholder="Name"
+                                        value={profile.name}
+                                        readOnly
+                                      />
+                                    </div>
+                                  </div>
                                 </div>
-                              </div>
-                              <div className="col-lg-4 col-md-4 col-sx-4">
-                                <div className="form-group">
-                                  <input
-                                    className="form-control"
-                                    type="text"
-                                    placeholder="CRM Id"
-                                    value={profile.crm_id}
-                                    readOnly
-                                  />
+                                <div className="row">
+                                  <div className="col-lg-2 col-md-2 col-sx-2">
+                                    <div className="form-group">
+                                      <label htmlFor="">Mobile Number</label>
+                                    </div>
+                                  </div>
+                                  <div className="col-lg-4 col-md-4 col-sx-4">
+                                    <div className="form-group">
+                                      <InputMask
+                                        mask="999 999 9999"
+                                        maskChar=""
+                                        className="form-control"
+                                        placeholder="Mobile Number(999 999 9999)"
+                                        value={profile.mobile}
+                                        readOnly
+                                      />
+                                    </div>
+                                  </div>
                                 </div>
-                              </div>
-                            </div>
-                            <div className="row">
-                              <div className="col-lg-2 col-md-2 col-sx-2">
-                                <div className="form-group">
-                                  <label htmlFor="">
-                                    Name
-                                  </label>
+                                <div className="row">
+                                  <div className="col-lg-2 col-md-2 col-sx-2">
+                                    <div className="form-group">
+                                      <label htmlFor="">Email ID</label>
+                                    </div>
+                                  </div>
+                                  <div className="col-lg-4 col-md-4 col-sx-4">
+                                    <div className="form-group">
+                                      <input
+                                        className="form-control"
+                                        type="text"
+                                        placeholder="Email ID"
+                                        value={profile.email}
+                                        readOnly
+                                      />
+                                    </div>
+                                  </div>
                                 </div>
-                              </div>
-                              <div className="col-lg-4 col-md-4 col-sx-4">
-                                <div className="form-group">
-                                  <input
-                                    className="form-control"
-                                    type="text"
-                                    placeholder="Name"
-                                    value={profile.name}
-                                    readOnly
-                                  />
+                                <div className="row">
+                                  <div className="col-lg-2 col-md-2 col-sx-2">
+                                    <div className="form-group">
+                                      <label htmlFor="">Role</label>
+                                    </div>
+                                  </div>
+                                  <div className="col-lg-4 col-md-4 col-sx-4">
+                                    <div className="form-group">
+                                      <input
+                                        className="form-control"
+                                        type="text"
+                                        placeholder="Email ID"
+                                        value={profile.role}
+                                        readOnly
+                                      />
+                                    </div>
+                                  </div>
                                 </div>
-                              </div>
-                            </div>
-                            <div className="row">
-                              <div className="col-lg-2 col-md-2 col-sx-2">
-                                <div className="form-group">
-                                  <label htmlFor="">
-                                    Mobile Number
-                                  </label>
+                                <div className="row">
+                                  <div className="col-lg-2 col-md-2 col-sx-2">
+                                    <div className="form-group">
+                                      <label htmlFor="">Status</label>
+                                    </div>
+                                  </div>
+                                  <div className="col-lg-4 col-md-4 col-sx-4">
+                                    <div className="form-group">
+                                      <input
+                                        className="form-control"
+                                        type="text"
+                                        placeholder="Email ID"
+                                        value={profile.status}
+                                        readOnly
+                                      />
+                                    </div>
+                                  </div>
                                 </div>
-                              </div>
-                              <div className="col-lg-4 col-md-4 col-sx-4">
-                                <div className="form-group">
-                                  <InputMask
-                                    mask="999 999 9999"
-                                    maskChar=""
-                                    className="form-control"
-                                    placeholder="Mobile Number(999 999 9999)"
-                                    value={profile.mobile}
-                                    readOnly
-                                  />
-                                </div>
-                              </div>
-                            </div>
-                            <div className="row">
-                              <div className="col-lg-2 col-md-2 col-sx-2">
-                                <div className="form-group">
-                                  <label htmlFor="">Email ID</label>
-                                </div>
-                              </div>
-                              <div className="col-lg-4 col-md-4 col-sx-4">
-                                <div className="form-group">
-                                  <input
-                                    className="form-control"
-                                    type="text"
-                                    placeholder="Email ID"
-                                    value={profile.email}
-                                    readOnly
-                                  />
-                                </div>
-                              </div>
-                            </div>
-                            <div className="row">
-                              <div className="col-lg-2 col-md-2 col-sx-2">
-                                <div className="form-group">
-                                  <label htmlFor="">Role</label>
-                                </div>
-                              </div>
-                              <div className="col-lg-4 col-md-4 col-sx-4">
-                                <div className="form-group">
-                                  <input
-                                    className="form-control"
-                                    type="text"
-                                    placeholder="Email ID"
-                                    value={profile.role}
-                                    readOnly
-                                  />
-                                </div>
-                              </div>
-                            </div>
-                            <div className="row">
-                              <div className="col-lg-2 col-md-2 col-sx-2">
-                                <div className="form-group">
-                                  <label htmlFor="">Status</label>
-                                </div>
-                              </div>
-                              <div className="col-lg-4 col-md-4 col-sx-4">
-                                <div className="form-group">
-                                  <input
-                                    className="form-control"
-                                    type="text"
-                                    placeholder="Email ID"
-                                    value={profile.status}
-                                    readOnly
-                                  />
-                                </div>
-                              </div>
-                            </div>
-                          </form>
-                          ))
-                        ) : (
-                          <p>No profile data found</p>
-                        )}
+                              </form>
+                            ))
+                          ) : (
+                            <p>No profile data found</p>
+                          )}
                         </div>
                       </div>
                     </div>
